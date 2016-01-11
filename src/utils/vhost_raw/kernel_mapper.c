cac6afebdb8ca8c9f06430e7e921076d49061cf2
Eyal Moscovici <eyal.moscovici@ravellosystems.com>
2016-01-11 23:59:39 +0200

    added prints for debugging

--- src/utils/vhost_raw/kernel_mapper.c
+++ src/utils/vhost_raw/kernel_mapper.c
@@ -14,22 +14,28 @@ void *kernel_remap(u64 kernel_address){
     char *mapped;
     void *ret;
 
+    printf("%s:%d", __func__, __LINE__);
     if(kernel_address < 0xc0000000UL) {
 	    fprintf(stderr, "illegal variable kernel address: 0x%llx\n", kernel_address);
 	    return NULL;
     }
 
+    printf("%s:%d", __func__, __LINE__);
     if((file = fopen("/dev/kmem", O_RDONLY)) == NULL) {
 	    fprintf(stderr, "failed open()ing /dev/kmem: %s\n", strerror(errno));
 	    return NULL;
     }
+    printf("%s:%d", __func__, __LINE__);
     if((mapped = mmap(NULL, mmap_size, PROT_READ, MAP_SHARED, fileno(file),
         mmap_offset)) ==  MAP_FAILED ) {
 	    fprintf(stderr, "user-kernel map failed: %s\n", strerror(errno));
 	    return NULL;
     }
+    printf("%s:%d", __func__, __LINE__);
     fclose(file);
+    printf("%s:%d", __func__, __LINE__);
 
     ret = (void *)(mapped + (kernel_address - mmap_offset));
+    printf("%s:%d", __func__, __LINE__);
     return ret;
 }
--- src/utils/vhost_raw/vhost_mapper.c
+++ src/utils/vhost_raw/vhost_mapper.c
@@ -9,46 +9,59 @@
 
 static const char * const stats_file_path = "/sys/class/vhost/%s/%s/stats_ptr";
 
-static void *__vhost_remap(const char * const fname){
+static void *__vhost_remap(const char * const fname)
+{
     FILE *file;
     ssize_t read;
     u64 kernel_address = 0UL;
 
+    printf("%s:%d", __func__, __LINE__);
     if((file = fopen(fname, O_RDONLY)) == NULL) {
         fprintf(stderr, "Couldn't open file: %s, error: %s\n", fname, strerror(errno));
         return NULL;
     }
+    printf("%s:%d", __func__, __LINE__);
     if ((read = fscanf(file, "%llx\n", &kernel_address)) <= 0) {
         fprintf(stderr, "Couldn't read file: %s, error: %s\n", fname, strerror(errno));
         return NULL;
     }
+    printf("%s:%d", __func__, __LINE__);
     fclose(file);
+    printf("%s:%d", __func__, __LINE__);
     return kernel_remap(kernel_address);
 }
 
-static void *vhost_remap(const char * const dir, const char * const id){
+static void *vhost_remap(const char * const dir, const char * const id)
+{
     char buf[256] = {0};
 
+    printf("%s:%d", __func__, __LINE__);
     if (snprintf(buf, 256, stats_file_path, dir, id) < 0){
        fprintf(stderr, "failed creating the file name.\n");
        return NULL;
     }
 
+    printf("%s:%d", __func__, __LINE__);
     if(access(buf, F_OK) == -1) {
         fprintf(stderr, "file not found: %s\n", buf);
         return NULL;
     }
+    printf("%s:%d", __func__, __LINE__);
     return __vhost_remap(buf);
 }
 
-struct vhost_worker_stats *remap_vhost_worker(const char * const id){
+struct vhost_worker_stats *remap_vhost_worker(const char * const id)
+{
+    printf("%s:%d", __func__, __LINE__);
     return (struct vhost_worker_stats *)vhost_remap("worker", id);
 }
 
-struct vhost_device_stats *remap_vhost_device(const char * const id){
+struct vhost_device_stats *remap_vhost_device(const char * const id)
+{
     return (struct vhost_device_stats *)vhost_remap("dev", id);
 }
 
-struct vhost_virtqueue_stats *remap_vhost_virtqueue(const char * const id){
+struct vhost_virtqueue_stats *remap_vhost_virtqueue(const char * const id)
+{
     return (struct vhost_virtqueue_stats *)vhost_remap("vq", id);
 }
