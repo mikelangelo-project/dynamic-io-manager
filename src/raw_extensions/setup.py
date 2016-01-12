9738f003f4883e5cf2fb8db3d7586f376fcd05f9
Eyal Moscovici <eyal.moscovici@ravellosystems.com>
2016-01-12 17:42:30 +0200

    added -lrt to uptime counter

--- src/raw_extensions/setup.py
+++ src/raw_extensions/setup.py
@@ -1,13 +1,12 @@
 from setuptools import Extension, setup, find_packages
 packages = find_packages()
 
-extensions = {
-    "vhost_raw": ["vhost_raw.c", "vhost_mapper.c", "copy_to_user.c"],
-    "uptime": ["uptime.c"],
-    "kernel_mapper": ["counter.c", "copy_to_user.c"]
-}
-ext_modules = [Extension(key, sources=sources)
-               for key, sources in extensions.items()]
+ext_modules = [
+    Extension("vhost_raw", sources=["vhost_raw.c", "vhost_mapper.c",
+                                    "copy_to_user.c"]),
+    Extension("uptime", sources=["uptime.c"], libraries=['rt']),
+    Extension("kernel_mapper", sources=["counter.c", "copy_to_user.c"])
+]
 print ext_modules
 setup(name="raw_extensions", author="Eyal Moscocvici", version="0.1",
       packages=packages, ext_modules=ext_modules)
